1e80941d037fdaf11b6e6a9cf54359edf92db6a2
VanyaMark <markova.vanya@gmail.com>
2024-06-19 10:30:28 +0200

    done refactoring styling with Tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -32,8 +32,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-900' >
+      <div className='flex flex-col gap-2 mb-6' >
 
           <Input
             label="Email"
@@ -54,9 +54,9 @@ export default function AuthInputs() {
             }
           />
 
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-600">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
